6d39bc180db4c832aaebcb3065a22b08bced2ac9
Alicja Sanestrzik <alicja.sanestrzik@gmail.com>
2020-03-02 00:00:30 +0100

    Fix

--- src/main/java/com/alicjasanestrzik/transactionstatistics/repository/TransactionRepository.java
+++ src/main/java/com/alicjasanestrzik/transactionstatistics/repository/TransactionRepository.java
@@ -6,15 +6,15 @@ import org.springframework.stereotype.Component;
 import javax.validation.Valid;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class TransactionRepository {
 
-    private List<Transaction> transactionList = Collections.synchronizedList(new LinkedList<>());
+    private List<Transaction> transactionList = Collections.synchronizedList(new ArrayList<>());
 
     public void add(@Valid Transaction transaction) {
         transactionList.add(transaction);
